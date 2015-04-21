64d2750e92bcb19fe5866458c961bedf7393b081
devonhg <devon.hugh.godfrey@gmail.com>
2015-04-21 13:42:30 -0400

    Added debug function to toolbox.

--- framework/class-toolbox.php
+++ framework/class-toolbox.php
@@ -33,4 +33,34 @@ class MYPLUGIN_DFIW_tb{
     	if ( substr($dir, -1) != "/"){ $dir .= "/"; }
     	return $dir; 
     }
+
+    //Creates a debug.txt file, enters values for testing. 
+    public static function debug( $entry , $value ){
+        $f = MYPLUGIN_HOME_DIR . "\debug.txt"; 
+        if ( file_exists( $f ) ){
+            $fc = file_get_contents( $f );
+
+            //Convert string to associative array
+            $mstr = explode("|",$fc);
+            $a = array();
+            foreach($mstr as $nstr )
+            {   
+                if (strpos($nstr, '=') !== FALSE ){
+                    $narr = explode("=",$nstr);
+                    $narr[0] = trim( str_replace("\x98","",$narr[0]) );
+                    $ytr[1] = trim( $narr[1] );
+                    $a[$narr[0]] =$ytr[1];
+                }
+            }
+
+            $a[$entry] = $value;
+
+            //Convert back to string
+            $fo = http_build_query($a, '', '|'."\n");
+            
+            file_put_contents( $f , $fo );
+        }else{
+            file_put_contents( $f , $entry . '=' . $value . " | " );
+        }
+    }
 }
